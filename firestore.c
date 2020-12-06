ead2c47dfa668255841b6131b2cb3b121b16b895
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2020-12-06 23:21:31 +0100

    Adding component

---
+++ firestore.c
@@ -0,0 +1,450 @@
+#include <string.h>
+#include "esp_log.h"
+#include "esp_http_client.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+#include "firestore.h"
+#include "firestore_config.h"
+
+#define TAG                                      "FIRESTORE"
+#define HTTP_RECEIVED_BIT                        BIT0
+#define HTTP_FAIL_BIT                            BIT1
+
+esp_err_t _firestore_http_event_handler(esp_http_client_event_t *pstEvent);
+
+typedef struct
+{
+  char tcPath[FIRESTORE_HTTP_PATH_SIZE];
+  uint32_t u32PathLen;
+  char tcHttpBody[FIRESTORE_HTTP_RX_BUF_SIZE];
+  uint32_t u32HttpBodyLen;
+  esp_http_client_handle_t pstHttpClient;
+  esp_http_client_config_t stHttpconfig;
+  EventGroupHandle_t stHttpEventGroup;
+  EventBits_t u32EventBits;
+}firestore_ctx_t;
+
+static firestore_ctx_t stCtx;
+
+firestore_err_t firestore_init(void)
+{
+#ifndef FIRESTORE_DEBUG
+  esp_log_level_set("FIRESTORE", ESP_LOG_NONE);
+#endif /* FIRESTORE_DEBUG */
+  memset(&stCtx, 0x00, sizeof(stCtx));
+  stCtx.stHttpEventGroup = xEventGroupCreate();
+  stCtx.stHttpconfig.host = FIRESTORE_HOSTNAME;
+  stCtx.stHttpconfig.buffer_size = FIRESTORE_HTTP_INTERNAL_RX_BUF_SIZE;
+  stCtx.stHttpconfig.buffer_size_tx = FIRESTORE_HTTP_INTERNAL_TX_BUF_SIZE;
+  stCtx.stHttpconfig.cert_pem = FIRESTORE_FIREBASE_CA_CERT_PEM;
+  stCtx.stHttpconfig.event_handler = _firestore_http_event_handler;
+  stCtx.stHttpconfig.transport_type = HTTP_TRANSPORT_OVER_SSL;
+  stCtx.stHttpconfig.user_data = stCtx.tcHttpBody;
+  return FIRESTORE_OK;
+}
+
+firestore_err_t firestore_get_collection(char *pcCollectionId,
+                                         char **ppcDocs,
+                                         uint32_t *pu32DocsLen)
+{
+  int16_t s16HttpCode;
+  int32_t s32Length;
+  firestore_err_t eRetVal;
+
+  eRetVal = FIRESTORE_OK;
+  if(pcCollectionId)
+  {
+    s32Length = snprintf(stCtx.tcPath,
+                         FIRESTORE_HTTP_PATH_SIZE,
+                         "/v1/projects/%s/databases/(default)/documents/%s?key=%s",
+                         FIRESTORE_FIREBASE_PROJECT_ID,
+                         pcCollectionId,
+                         FIRESTORE_FIREBASE_API_KEY);
+    if(s32Length > 0)
+    {
+      stCtx.stHttpconfig.path = stCtx.tcPath;
+      ESP_LOGI(TAG, "HTTP path: %s", stCtx.stHttpconfig.path);
+      stCtx.pstHttpClient = esp_http_client_init(&stCtx.stHttpconfig);
+      if(ESP_OK == esp_http_client_perform(stCtx.pstHttpClient))
+      {
+        stCtx.u32EventBits = xEventGroupWaitBits(stCtx.stHttpEventGroup,
+                                                 HTTP_RECEIVED_BIT | HTTP_FAIL_BIT,
+                                                 pdFALSE,
+                                                 pdFALSE,
+                                                 portMAX_DELAY);
+        if(stCtx.u32EventBits & HTTP_RECEIVED_BIT)
+        {
+          s16HttpCode = esp_http_client_get_status_code(stCtx.pstHttpClient);
+          ESP_LOGI(TAG,
+                   "HTTP code: %d, content_length: %d",
+                   s16HttpCode,
+                   esp_http_client_get_content_length(stCtx.pstHttpClient));
+          if(200 != s16HttpCode)
+          {
+            ESP_LOGE(TAG, "Firestore REST API call failed with HTTP code: %d", s16HttpCode);
+            eRetVal = FIRESTORE_ERR_HTTP;
+          }
+          else
+          {
+            *ppcDocs = stCtx.tcHttpBody;
+            *pu32DocsLen = stCtx.u32HttpBodyLen;
+
+          }
+        }
+        else if(stCtx.u32EventBits & HTTP_FAIL_BIT)
+        {
+          eRetVal = FIRESTORE_ERR_HTTP;
+        }
+      }
+      else
+      {
+        eRetVal = FIRESTORE_ERR_HTTP;
+      }
+    }
+    else
+    {
+      eRetVal = FIRESTORE_ERR;
+    }
+  }
+  else
+  {
+    eRetVal = FIRESTORE_ERR_ARG;
+  }
+  vEventGroupDelete(stCtx.stHttpEventGroup);
+  return eRetVal;
+}
+
+
+firestore_err_t firestore_get_document(char *pcCollectionId,
+                                       char *pcDocumentId,
+                                       char **ppcDocument,
+                                       uint32_t *pu32DocumentLen)
+{
+  int16_t s16HttpCode;
+  int32_t s32Length;
+  firestore_err_t eRetVal;
+
+  eRetVal = FIRESTORE_OK;
+  if(pcCollectionId && pcDocumentId)
+  {
+    s32Length = snprintf(stCtx.tcPath,
+                         FIRESTORE_HTTP_PATH_SIZE,
+                         "/v1/projects/%s/databases/(default)/documents/%s/%s?key=%s",
+                         FIRESTORE_FIREBASE_PROJECT_ID,
+                         pcCollectionId,
+                         pcDocumentId,
+                         FIRESTORE_FIREBASE_API_KEY);
+    if(s32Length > 0)
+    {
+      stCtx.stHttpconfig.path = stCtx.tcPath;
+      ESP_LOGI(TAG, "HTTP path: %s", stCtx.stHttpconfig.path);
+      stCtx.pstHttpClient = esp_http_client_init(&stCtx.stHttpconfig);
+      if(ESP_OK == esp_http_client_perform(stCtx.pstHttpClient))
+      {
+        stCtx.u32EventBits = xEventGroupWaitBits(stCtx.stHttpEventGroup,
+                                                 HTTP_RECEIVED_BIT | HTTP_FAIL_BIT,
+                                                 pdFALSE,
+                                                 pdFALSE,
+                                                 portMAX_DELAY);
+        if(stCtx.u32EventBits & HTTP_RECEIVED_BIT)
+        {
+          s16HttpCode = esp_http_client_get_status_code(stCtx.pstHttpClient);
+          ESP_LOGI(TAG,
+                   "HTTP code: %d, content_length: %d",
+                   s16HttpCode,
+                   esp_http_client_get_content_length(stCtx.pstHttpClient));
+          if(200 != s16HttpCode)
+          {
+            ESP_LOGE(TAG, "Firestore REST API call failed with HTTP code: %d", s16HttpCode);
+            eRetVal = FIRESTORE_ERR_HTTP;
+          }
+          else
+          {
+            *ppcDocument = stCtx.tcHttpBody;
+            *pu32DocumentLen = stCtx.u32HttpBodyLen;
+          }
+        }
+        else if(stCtx.u32EventBits & HTTP_FAIL_BIT)
+        {
+          eRetVal = FIRESTORE_ERR_HTTP;
+        }
+      }
+      else
+      {
+        eRetVal = FIRESTORE_ERR_HTTP;
+      }
+    }
+    else
+    {
+      eRetVal = FIRESTORE_ERR;
+    }
+  }
+  else
+  {
+    eRetVal = FIRESTORE_ERR_ARG;
+  }
+  vEventGroupDelete(stCtx.stHttpEventGroup);
+  return eRetVal;
+}
+
+firestore_err_t firestore_add_document(char *pcCollectionId,
+                                       char *pcDocumentId,
+                                       char *pcDocument,
+                                       uint32_t *pu32DocumentLen)
+{
+  int16_t s16HttpCode;
+  int32_t s32Length;
+  firestore_err_t eRetVal;
+
+  eRetVal = FIRESTORE_OK;
+  if(pcCollectionId && pcDocumentId)
+  {
+    s32Length = snprintf(stCtx.tcPath,
+                         FIRESTORE_HTTP_PATH_SIZE,
+                         "/v1/projects/%s/databases/(default)/documents/%s?documentId=%s&key=%s",
+                         FIRESTORE_FIREBASE_PROJECT_ID,
+                         pcCollectionId,
+                         pcDocumentId,
+                         FIRESTORE_FIREBASE_API_KEY);
+    if(s32Length > 0)
+    {
+      stCtx.stHttpconfig.path = stCtx.tcPath;
+      ESP_LOGI(TAG, "HTTP path: %s", stCtx.stHttpconfig.path);
+      stCtx.pstHttpClient = esp_http_client_init(&stCtx.stHttpconfig);
+      esp_http_client_set_method(stCtx.pstHttpClient, HTTP_METHOD_POST);
+      esp_http_client_set_header(stCtx.pstHttpClient, "Content-Type", "application/json");
+      esp_http_client_set_post_field(stCtx.pstHttpClient, pcDocument, strlen(pcDocument));
+      if(ESP_OK == esp_http_client_perform(stCtx.pstHttpClient))
+      {
+        stCtx.u32EventBits = xEventGroupWaitBits(stCtx.stHttpEventGroup,
+                                                 HTTP_RECEIVED_BIT | HTTP_FAIL_BIT,
+                                                 pdFALSE,
+                                                 pdFALSE,
+                                                 portMAX_DELAY);
+        if(stCtx.u32EventBits & HTTP_RECEIVED_BIT)
+        {
+          s16HttpCode = esp_http_client_get_status_code(stCtx.pstHttpClient);
+          ESP_LOGI(TAG,
+                   "HTTP PATCH Status = %d, content_length = %d",
+                   esp_http_client_get_status_code(stCtx.pstHttpClient),
+                   esp_http_client_get_content_length(stCtx.pstHttpClient));
+          if(200 != s16HttpCode)
+          {
+            ESP_LOGE(TAG, "Firestore REST API call failed with HTTP code: %d", s16HttpCode);
+            eRetVal = FIRESTORE_ERR_HTTP;
+          }
+          else
+          {
+            pcDocument = stCtx.tcHttpBody;
+            *pu32DocumentLen = stCtx.u32HttpBodyLen;
+          }
+        }
+        else if(stCtx.u32EventBits & HTTP_FAIL_BIT)
+        {
+          eRetVal = FIRESTORE_ERR_HTTP;
+        }
+      }
+      else
+      {
+        eRetVal = FIRESTORE_ERR_HTTP;
+      }
+    }
+    else
+    {
+      eRetVal = FIRESTORE_ERR;
+    }
+  }
+  else
+  {
+    eRetVal = FIRESTORE_ERR_ARG;
+  }
+  vEventGroupDelete(stCtx.stHttpEventGroup);
+  return eRetVal;
+}
+
+firestore_err_t firestore_update_document(char *pcCollectionId,
+                                          char *pcDocumentId,
+                                          char *pcDocument,
+                                          uint32_t *pu32DocumentLen)
+{
+  int16_t s16HttpCode;
+  int32_t s32Length;
+  firestore_err_t eRetVal;
+
+  eRetVal = FIRESTORE_OK;
+  if(pcCollectionId && pcDocumentId)
+  {
+    s32Length = snprintf(stCtx.tcPath,
+                         FIRESTORE_HTTP_PATH_SIZE,
+                         "/v1/projects/%s/databases/(default)/documents/%s/%s?key=%s",
+                         FIRESTORE_FIREBASE_PROJECT_ID,
+                         pcCollectionId,
+                         pcDocumentId,
+                         FIRESTORE_FIREBASE_API_KEY);
+    if(s32Length > 0)
+    {
+      stCtx.stHttpconfig.path = stCtx.tcPath;
+      ESP_LOGI(TAG, "HTTP path: %s", stCtx.stHttpconfig.path);
+      stCtx.pstHttpClient = esp_http_client_init(&stCtx.stHttpconfig);
+      esp_http_client_set_method(stCtx.pstHttpClient, HTTP_METHOD_PATCH);
+      esp_http_client_set_header(stCtx.pstHttpClient, "Content-Type", "application/json");
+      esp_http_client_set_post_field(stCtx.pstHttpClient, pcDocument, strlen(pcDocument));
+      if(ESP_OK == esp_http_client_perform(stCtx.pstHttpClient))
+      {
+        stCtx.u32EventBits = xEventGroupWaitBits(stCtx.stHttpEventGroup,
+                                                 HTTP_RECEIVED_BIT | HTTP_FAIL_BIT,
+                                                 pdFALSE,
+                                                 pdFALSE,
+                                                 portMAX_DELAY);
+        if(stCtx.u32EventBits & HTTP_RECEIVED_BIT)
+        {
+          s16HttpCode = esp_http_client_get_status_code(stCtx.pstHttpClient);
+          ESP_LOGI(TAG,
+                   "HTTP PATCH Status = %d, content_length = %d",
+                   esp_http_client_get_status_code(stCtx.pstHttpClient),
+                   esp_http_client_get_content_length(stCtx.pstHttpClient));
+          if(200 != s16HttpCode)
+          {
+            ESP_LOGE(TAG, "Firestore REST API call failed with HTTP code: %d", s16HttpCode);
+            eRetVal = FIRESTORE_ERR_HTTP;
+          }
+          else
+          {
+            pcDocument = stCtx.tcHttpBody;
+            *pu32DocumentLen = stCtx.u32HttpBodyLen;
+          }
+        }
+        else if(stCtx.u32EventBits & HTTP_FAIL_BIT)
+        {
+          eRetVal = FIRESTORE_ERR_HTTP;
+        }
+      }
+      else
+      {
+        eRetVal = FIRESTORE_ERR_HTTP;
+      }
+    }
+    else
+    {
+      eRetVal = FIRESTORE_ERR;
+    }
+  }
+  else
+  {
+    eRetVal = FIRESTORE_ERR_ARG;
+  }
+  vEventGroupDelete(stCtx.stHttpEventGroup);
+  return eRetVal;
+}
+
+firestore_err_t firestore_delete_document(char *pcCollectionId, char *pcDocumentId)
+{
+  int16_t s16HttpCode;
+  int32_t s32Length;
+  firestore_err_t eRetVal;
+
+  eRetVal = FIRESTORE_OK;
+  if(pcCollectionId && pcDocumentId)
+  {
+    s32Length = snprintf(stCtx.tcPath,
+                         FIRESTORE_HTTP_PATH_SIZE,
+                         "/v1/projects/%s/databases/(default)/documents/%s/%s?key=%s",
+                         FIRESTORE_FIREBASE_PROJECT_ID,
+                         pcCollectionId,
+                         pcDocumentId,
+                         FIRESTORE_FIREBASE_API_KEY);
+    if(s32Length > 0)
+    {
+      stCtx.stHttpconfig.path = stCtx.tcPath;
+      ESP_LOGI(TAG, "HTTP path: %s", stCtx.stHttpconfig.path);
+      stCtx.pstHttpClient = esp_http_client_init(&stCtx.stHttpconfig);
+      esp_http_client_set_method(stCtx.pstHttpClient, HTTP_METHOD_DELETE);
+      if(ESP_OK == esp_http_client_perform(stCtx.pstHttpClient))
+      {
+        stCtx.u32EventBits = xEventGroupWaitBits(stCtx.stHttpEventGroup,
+                                                 HTTP_RECEIVED_BIT | HTTP_FAIL_BIT,
+                                                 pdFALSE,
+                                                 pdFALSE,
+                                                 portMAX_DELAY);
+        if(stCtx.u32EventBits & HTTP_RECEIVED_BIT)
+        {
+          s16HttpCode = esp_http_client_get_status_code(stCtx.pstHttpClient);
+          ESP_LOGI(TAG,
+                   "HTTP code: %d, content_length: %d",
+                   s16HttpCode,
+                   esp_http_client_get_content_length(stCtx.pstHttpClient));
+          if(200 != s16HttpCode)
+          {
+            ESP_LOGE(TAG, "Firestore REST API call failed with HTTP code: %d", s16HttpCode);
+            eRetVal = FIRESTORE_ERR_HTTP;
+          }
+          else
+          {
+            ESP_LOGI(TAG, "Document deleted successfully");
+          }
+        }
+        else if(stCtx.u32EventBits & HTTP_FAIL_BIT)
+        {
+          eRetVal = FIRESTORE_ERR_HTTP;
+        }
+      }
+      else
+      {
+        eRetVal = FIRESTORE_ERR_HTTP;
+      }
+    }
+    else
+    {
+      eRetVal = FIRESTORE_ERR;
+    }
+  }
+  else
+  {
+    eRetVal = FIRESTORE_ERR_ARG;
+  }
+  vEventGroupDelete(stCtx.stHttpEventGroup);
+  return eRetVal;
+}
+
+esp_err_t _firestore_http_event_handler(esp_http_client_event_t *pstEvent)
+{
+  esp_err_t s32RetVal;
+
+  s32RetVal = ESP_OK;
+  switch(pstEvent->event_id)
+  {
+  case HTTP_EVENT_ERROR:
+    ESP_LOGI(TAG, "HTTP error");
+    xEventGroupSetBits(stCtx.stHttpEventGroup, HTTP_FAIL_BIT);
+    break;
+  case HTTP_EVENT_ON_CONNECTED:
+    ESP_LOGI(TAG, "HTTP connected to server");
+    break;
+  case HTTP_EVENT_HEADERS_SENT:
+    ESP_LOGI(TAG, "All HTTP headers are sent to server");
+    break;
+  case HTTP_EVENT_ON_HEADER:
+    ESP_LOGI(TAG, "HTTP header received");
+    break;
+  case HTTP_EVENT_ON_DATA:
+    /* If user_data buffer is configured, copy the response into it */
+    if(pstEvent->user_data)
+    {
+      strncpy(pstEvent->user_data+stCtx.u32HttpBodyLen,
+              (char*)pstEvent->data,
+              pstEvent->data_len);
+      stCtx.u32HttpBodyLen += pstEvent->data_len;
+    }
+    /* Else you can copy the response into a global HTTP buffer */
+    break;
+  case HTTP_EVENT_ON_FINISH:
+    ESP_LOGI(TAG, "HTTP session is finished");
+    xEventGroupSetBits(stCtx.stHttpEventGroup, HTTP_RECEIVED_BIT);
+    break;
+  case HTTP_EVENT_DISCONNECTED:
+    ESP_LOGI(TAG, "HTTP connection is closed");
+    break;
+  }
+  return s32RetVal;
+}
---
+++ firestore_config.h
@@ -0,0 +1,38 @@
+#ifndef _FIRESTORE_CONFIG_H_
+#define _FIRESTORE_CONFIG_H_
+
+#define FIRESTORE_HOSTNAME                       "firestore.googleapis.com"
+#define FIRESTORE_HTTP_INTERNAL_TX_BUF_SIZE      1024
+#define FIRESTORE_HTTP_INTERNAL_RX_BUF_SIZE      1024
+#define FIRESTORE_HTTP_RX_BUF_SIZE               1024*2
+#define FIRESTORE_HTTP_PATH_SIZE                 256
+#define FIRESTORE_FIREBASE_PROJECT_ID            "PUT_YOUR_FIREBASE_PROJECT_ID_HERE"
+#define FIRESTORE_FIREBASE_API_KEY               "PUT_YOUR_FIREBASE_API_KEY_HERE"
+/* openssl s_client -showcerts -verify 5 -connect edgecert.googleapis.com:443 < /dev/null */
+#define FIRESTORE_FIREBASE_CA_CERT_PEM                                                            \
+                                             "-----BEGIN CERTIFICATE-----\r\n"                    \
+                           "MIIESjCCAzKgAwIBAgINAeO0mqGNiqmBJWlQuDANBgkqhkiG9w0BAQsFADBMMSAw\r\n" \
+                           "HgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEGA1UEChMKR2xvYmFs\r\n" \
+                           "U2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjAeFw0xNzA2MTUwMDAwNDJaFw0yMTEy\r\n" \
+                           "MTUwMDAwNDJaMEIxCzAJBgNVBAYTAlVTMR4wHAYDVQQKExVHb29nbGUgVHJ1c3Qg\r\n" \
+                           "U2VydmljZXMxEzARBgNVBAMTCkdUUyBDQSAxTzEwggEiMA0GCSqGSIb3DQEBAQUA\r\n" \
+                           "A4IBDwAwggEKAoIBAQDQGM9F1IvN05zkQO9+tN1pIRvJzzyOTHW5DzEZhD2ePCnv\r\n" \
+                           "UA0Qk28FgICfKqC9EksC4T2fWBYk/jCfC3R3VZMdS/dN4ZKCEPZRrAzDsiKUDzRr\r\n" \
+                           "mBBJ5wudgzndIMYcLe/RGGFl5yODIKgjEv/SJH/UL+dEaltN11BmsK+eQmMF++Ac\r\n" \
+                           "xGNhr59qM/9il71I2dN8FGfcddwuaej4bXhp0LcQBbjxMcI7JP0aM3T4I+DsaxmK\r\n" \
+                           "FsbjzaTNC9uzpFlgOIg7rR25xoynUxv8vNmkq7zdPGHXkxWY7oG9j+JkRyBABk7X\r\n" \
+                           "rJfoucBZEqFJJSPk7XA0LKW0Y3z5oz2D0c1tJKwHAgMBAAGjggEzMIIBLzAOBgNV\r\n" \
+                           "HQ8BAf8EBAMCAYYwHQYDVR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMBIGA1Ud\r\n" \
+                           "EwEB/wQIMAYBAf8CAQAwHQYDVR0OBBYEFJjR+G4Q68+b7GCfGJAboOt9Cf0rMB8G\r\n" \
+                           "A1UdIwQYMBaAFJviB1dnHB7AagbeWbSaLd/cGYYuMDUGCCsGAQUFBwEBBCkwJzAl\r\n" \
+                           "BggrBgEFBQcwAYYZaHR0cDovL29jc3AucGtpLmdvb2cvZ3NyMjAyBgNVHR8EKzAp\r\n" \
+                           "MCegJaAjhiFodHRwOi8vY3JsLnBraS5nb29nL2dzcjIvZ3NyMi5jcmwwPwYDVR0g\r\n" \
+                           "BDgwNjA0BgZngQwBAgIwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly9wa2kuZ29vZy9y\r\n" \
+                           "ZXBvc2l0b3J5LzANBgkqhkiG9w0BAQsFAAOCAQEAGoA+Nnn78y6pRjd9XlQWNa7H\r\n" \
+                           "TgiZ/r3RNGkmUmYHPQq6Scti9PEajvwRT2iWTHQr02fesqOqBY2ETUwgZQ+lltoN\r\n" \
+                           "FvhsO9tvBCOIazpswWC9aJ9xju4tWDQH8NVU6YZZ/XteDSGU9YzJqPjY8q3MDxrz\r\n" \
+                           "mqepBCf5o8mw/wJ4a2G6xzUr6Fb6T8McDO22PLRL6u3M4Tzs3A2M1j6bykJYi8wW\r\n" \
+                           "IRdAvKLWZu/axBVbzYmqmwkm5zLSDW5nIAJbELCQCZwMH56t2Dvqofxs6BBcCFIZ\r\n" \
+                           "USpxu6x6td0V7SvJCCosirSmIatj/9dSSVDQibet8q/7UK4v4ZUN80atnZz1yg==\r\n" \
+                                             "-----END CERTIFICATE-----\r\n"
+#endif /* _FIRESTORE_CONFIG_H_ */
