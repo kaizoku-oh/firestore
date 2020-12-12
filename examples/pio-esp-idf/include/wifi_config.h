a89854cb9707253711103f6e6bb922af0a5dc6a3
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2020-12-12 13:37:20 +0100

    Cleaned example

--- examples/pio-esp-idf/include/wifi_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef _WIFI_CONFIG_H_
-#define _WIFI_CONFIG_H_
-
-#define WIFI_SSID                                "PUT_YOUR_WIFI_SSID_HERE"
-#define WIFI_PASS                                "PUT_YOUR_WIFI_PASS_HERE"
-
-#endif /* _WIFI_CONFIG_H_ */
